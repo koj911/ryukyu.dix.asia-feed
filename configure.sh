cc4ab60898109538b20095686df13d00fba0da5b
koj911 <59871282+koj911@users.noreply.github.com>
2024-07-31 19:41:12 +0900

    Update configure.sh
    port:30187がfork元のairplanes.liveのスクリプトと衝突していた。
    port:31015も衝突しているかもしれない。

--- configure.sh
+++ configure.sh
@@ -131,7 +131,7 @@ UAT_INPUT="127.0.0.1:30978"
 RESULTS="--results beast,connect,127.0.0.1:30104"
 RESULTS2="--results basestation,listen,31015"
 RESULTS3="--results beast,listen,30157"
-RESULTS4="--results beast,connect,127.0.0.1:30187"
+RESULTS4="--results beast,connect,127.0.0.1:30195"
 # add --privacy between the quotes below to disable having the feed name shown on the mlat map
 # (position is never shown accurately no matter the settings)
 PRIVACY=""
@@ -139,7 +139,7 @@ INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER="ryukyu.dix.asia:31090"
 TARGET="--net-connector ryukyu.dix.asia,30004,beast_reduce_plus_out,ryukyu.dix.asia,64004"
-NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30187 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/ryukyu.dix.asia/ryukyu-uuid"
+NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30195 --net-bo-port 0 --net-ri-port 0 --write-json-every 1 --uuid-file /usr/local/share/ryukyu.dix.asia/ryukyu-uuid"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
 EOF
 
